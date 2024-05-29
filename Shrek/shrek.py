acc08ff2c82d6bdb703a7b212a6711b0024d6264
mbecklas <75644072+mbecklas@users.noreply.github.com>
2024-05-29 18:00:06 -0300

    shrek.py

--- Shrek/shrek.py
+++ Shrek/shrek.py
@@ -2,15 +2,18 @@ import time
 import pyautogui
 import sys
 
-def SendScript(filename: str):
-	time.sleep(2)
-	with open(filename) as f:
-		lines = f.readlines()
-	for line in lines:
-		pyautogui.typewrite(line.strip())
-		pyautogui.press('enter')
-
-if __name__ == "__main__":
-	if(len(sys.argv) < 2):
-		SendScript("script.txt")
-	SendScript(sys.argv[1])
+keeprunning = true
+while keeprunning:
+	def SendScript(filename: str):
+		time.sleep(2)
+		with open(filename) as f:
+			lines = f.readlines()
+		for line in lines:
+			pyautogui.typewrite(line.strip())
+			pyautogui.press('enter')
+			time.sleep(0.5)
+	
+	if __name__ == "__main__":
+		if(len(sys.argv) < 2):
+			SendScript("script.txt")
+		SendScript(sys.argv[1])
